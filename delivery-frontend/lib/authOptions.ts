fe531215a023a518db7094ed59532b617f4cb060
Francisco <franciscomonteirov@hotmail.com>
2024-11-24 21:00:53 +0000

    Add supabase integration with nextauth

--- delivery-frontend/lib/authOptions.ts
+++ delivery-frontend/lib/authOptions.ts
@@ -1,4 +1,8 @@
 import GoogleProvider from "next-auth/providers/google"
+import { SupabaseAdapter } from "@auth/supabase-adapter"
+import NextAuth from "next-auth"
+
+
 
 import { NextAuthOptions } from "next-auth"
 
@@ -8,7 +12,11 @@ export const authOptions: NextAuthOptions = {
         clientId: process.env.GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
-  ]
+  ],
+  adapter: SupabaseAdapter({
+    url: process.env.SUPABASE_URL as string,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string
+  })
 }
 
 
